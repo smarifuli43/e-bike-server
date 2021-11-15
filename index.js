7202485bd072e6cc9e88842ac06c92c5f283e7e0
Md Ariful Islam <smarifulislami43@gmail.com>
2021-11-16 04:00:07 +0600

    delete

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 
-
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -114,12 +112,41 @@ async function run() {
       res.json(result);
     });
     // GET MY ORDER
-    app.get('/orders', async (req, res) => {
+    app.get('/myorders', async (req, res) => {
       const email = req.query.email;
+      console.log(email);
       const query = { email: email };
       const cursor = orderCollection.find(query);
       const orders = await cursor.toArray();
-      res.json(orders);
+      console.log(orders);
+      res.send(orders);
+    });
+    // DELETE ORDERS
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
+    // GET ALL ORDER
+    app.get('/orders', async (req, res) => {
+      const cursor = orderCollection.find({});
+      const orders = await cursor.toArray();
+
+      res.send(orders);
+    });
+    // UPDATE status API
+    app.put('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedOrder = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: updatedOrder[0].status,
+        },
+      };
+      const result = await orderCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
   } finally {
     // await client.close()
