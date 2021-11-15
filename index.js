7ef12f2ad9a4e7516a9516ea7d9c154c2b13ebe5
Md Ariful Islam <smarifulislami43@gmail.com>
2021-11-15 22:24:48 +0600

    admin

--- index.js
+++ index.js
@@ -3,35 +3,12 @@ const cors = require('cors');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-/* 
-// https://i.ibb.co/zRZLtQ5/Mountain-Bike-Semi-Electric.png
-// https://i.ibb.co/2qgkfcC/Mountain-Bike-With-Middle-Single-Shock.png
-// https://i.ibb.co/g3fB5Yk/Ultimate-Triathlon-Super-Bike.png
-// https://i.ibb.co/xzW4T1L/Vintage-Classic-Duo-Rare.png
-// https://i.ibb.co/Nty73wQ/Vintage-Classic-Green.png
-// https://i.ibb.co/LRxBTZT/Vintage-Classic-With-Basket-Rare.png
-// https://i.ibb.co/9tjpzrw/Vintage-Classic-With-Basket.png
-// https://i.ibb.co/sJPYQ4Z/Adventure-White-Solid.png
-// https://i.ibb.co/D7mSXFX/Classic-Green-Hi.png
- */
-
-// const i = [
-//   {
-//     name: 'Vintage Classic With Basket',
-//     img: 'https://i.ibb.co/9tjpzrw/Vintage-Classic-With-Basket.png',
-//     price: 599,
-//     description: ' This is a world class vintage bike, people like it for its vintage but smartest design. ',
-    
-//   },
-// ];
-
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.razkq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -47,6 +24,7 @@ async function run() {
 
     const database = client.db('ebike');
     const productsCollection = database.collection('products');
+    const usersCollection = database.collection('users');
 
     // GET ALL PRODUCTS API
     app.get('/products', async (req, res) => {
@@ -61,14 +39,53 @@ async function run() {
       const result = await productsCollection.insertOne(product);
       res.json(result);
     });
-  }
-  finally {
+
+    //post users
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+    // post user for google sign in
+    app.put('/users', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    // API for make ADMIN
+    app.put('/users/admin', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: 'admin' } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+    // api for getting admin info
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === 'admin') {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+  } finally {
     // await client.close()
   }
 }
 
 run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
   res.send('E-bike server running');
